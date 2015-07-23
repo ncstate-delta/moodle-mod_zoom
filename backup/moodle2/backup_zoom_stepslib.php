161548ae809b8283e1f54efa14f76d258c75dbac
Nathan Chou <nchou0@ucla.edu>
2015-07-23 14:53:08 -0700

    Add Moodle license to a file.

--- backup/moodle2/backup_zoom_stepslib.php
+++ backup/moodle2/backup_zoom_stepslib.php
@@ -1,10 +1,18 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// This file is part of the Zoom plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Define the complete zoom structure for backup, with file and id annotations
