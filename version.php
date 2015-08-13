c7cc21b1851c420cd415aacc39183ed47446e5db
Rex Lorenzo <rex@oid.ucla.edu>
2015-08-13 13:12:02 -0700

    Made plugin STABLE and removed errant file.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
 $plugin->version = 2015072000;
-$plugin->release = 'v0.0';
+$plugin->release = 'v1.0';
 $plugin->requires = 2014051200;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
