ncstate-delta/moodle-mod_zoom

commit 6d93189203b0b9ca9c1a9b502d7cba3b37559f21

Improve interface in response to feedback. * Add help buttons for recurring meetings and join before host. * Change some language strings. * Use a button-like link for start/join meeting on view page.

mod_form.php
@@ -73,6 +73,7 @@ class mod_zoom_mod_form extends moodleform_mod {
         $mform->addElement('advcheckbox', 'type', get_string('recurringmeeting', 'zoom'), '',
                 null, array(ZOOM_SCHEDULED_MEETING, ZOOM_RECURRING_MEETING));
         $mform->setDefault('type', ZOOM_SCHEDULED_MEETING);
+        $mform->addHelpButton('type', 'recurringmeeting', 'zoom');
 
         // Add password.
         $mform->addElement('passwordunmask', 'password', get_string('password', 'zoom'), array('maxlength' => '10'));
@@ -103,10 +104,10 @@ class mod_zoom_mod_form extends moodleform_mod {
 
         // Add meeting options.
         $mform->addGroup(array(
-            // TODO: meeting password.
             // Join before host.
             $mform->createElement('checkbox', 'option_jbh', '', get_string('option_jbh', 'zoom'))
-        ), null, get_string('meetingoptions', 'zoom'));
+        ), 'meetingoptions', get_string('meetingoptions', 'zoom'));
+        $mform->addHelpButton('meetingoptions', 'meetingoptions', 'zoom');
 
         // Add meeting id.
         $mform->addElement('hidden', 'meeting_id', -1);

index.php
@@ -26,8 +26,6 @@ require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
 require_once(dirname(__FILE__).'/locallib.php');
 
-$config = get_config('mod_zoom');
-
 $id = required_param('id', PARAM_INT); // Course.
 
 $course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
@@ -111,7 +109,7 @@ $modinfo = get_fast_modinfo($course);
 $cms = $modinfo->instances['zoom'];
 foreach ($zooms as $z) {
     $row = array();
-    $expired = $z->type != ZOOM_RECURRING_MEETING && $z->start_time + $z->duration < $now;
+    list($inprogress, $available, $finished) = zoom_get_state($z);
 
     $cm = $cms[$z->id];
     if ($usesections && isset($cm->sectionnum)) {
@@ -124,16 +122,10 @@ foreach ($zooms as $z) {
     $displaytime = ($z->type == ZOOM_RECURRING_MEETING) ?
             get_string('recurringmeetinglong', 'mod_zoom') : userdate($z->start_time);
 
-    if ($expired) {
+    if ($finished) {
         $row[2] = $displaytime;
         $oldtable->data[] = $row;
     } else {
-        $firstavailable = $z->start_time - ($config->firstabletojoin * 60);
-        $lastavailable = $z->start_time + $z->duration;
-        $inprogress = ($firstavailable < $now && $now < $lastavailable);
-
-        $available = $z->type == ZOOM_RECURRING_MEETING || $inprogress;
-
         if ($inprogress) {
             $label = html_writer::tag('span', $strmeetingstarted,
                     array('class' => 'label label-info zoom-info'));
@@ -147,16 +139,16 @@ foreach ($zooms as $z) {
 
         if ($available) {
             if ($zoomuserid === false || $zoomuserid != $z->host_id) {
-                $button = html_writer::tag('button', $strjoin,
+                $buttonhtml = html_writer::tag('button', $strjoin,
                         array('type' => 'submit', 'class' => 'btn btn-primary'));
-                $aurl = new moodle_url($z->join_url,
-                        array('uname' => $USER->firstname.' '.$USER->lastname));
+                $aurl = new moodle_url('/mod/zoom/loadmeeting.php', array('id' => $cm->id));
             } else {
-                $button = html_writer::tag('button', $strstart,
+                $buttonhtml = html_writer::tag('button', $strstart,
                         array('type' => 'submit', 'class' => 'btn btn-success'));
-                $aurl = $z->start_url;
+                $aurl = new moodle_url($z->start_url);
             }
-            $row[4] = html_writer::tag('form', $button, array('action' => $aurl));
+            $buttonhtml .= html_writer::input_hidden_params($aurl);
+            $row[4] = html_writer::tag('form', $buttonhtml, array('action' => $aurl->out_omit_querystring()));
         } else {
             $row[4] = '--';
         }

lang/en/zoom.php
@@ -53,15 +53,16 @@ $string['join_meeting'] = 'Join Meeting';
 $string['meeting_expired'] = 'Expired / Deleted';
 $string['meeting_finished'] = 'Finished';
 $string['meeting_not_started'] = 'Not started';
-$string['meetingoptions'] = 'Meeting options';
+$string['meetingoptions'] = 'Meeting option';
+$string['meetingoptions_help'] = '*Join before host* allows attendees to join the meeting before the host joins '
+        . 'or when the host cannot attend the meeting.';
 $string['meeting_started'] = 'In progress';
 $string['meeting_time'] = 'Start Time';
 $string['minutes'] = '{$a} min';
 $string['modulename'] = 'Zoom meeting';
 $string['modulenameplural'] = 'Zoom Meetings';
-$string['modulename_help'] = 'Zoom is the web and app based video conferencing '
-        . 'service. Zoom offers voice and video conferencing, screen sharing, '
-        . 'and co-annotation. Up to 25 attendees can be in a Zoom meeting.';
+$string['modulename_help'] = 'Zoom is a video and web conferencing platform that gives '
+        . 'authorized users the ability to host online meetings with up to 25 participants.';
 $string['newmeetings'] = 'New Meetings';
 $string['no'] = 'No';
 $string['nozooms'] = 'No meetings';
@@ -77,6 +78,7 @@ $string['passwordprotected'] = 'Password Protected';
 $string['pluginadministration'] = 'Manage Zoom meeting';
 $string['pluginname'] = 'Zoom meeting';
 $string['recurringmeeting'] = 'Recurring meeting';
+$string['recurringmeeting_help'] = 'Meeting with no end date';
 $string['recurringmeetinglong'] = 'Recurring meeting (meeting with no end date or time)';
 $string['requirepassword'] = 'Require meeting password';
 $string['start'] = 'Start';

locallib.php
@@ -118,3 +118,24 @@ function zoom_update_records(Traversable $zooms) {
         rebuild_course_cache($course, true);
     }
 }
+
+/**
+ * Determine if a zoom meeting is in progress, is available, and/or is finished.
+ *
+ * @param stdClass $zoom
+ * @return array Array of booleans: [in progress, available, finished].
+ */
+function zoom_get_state($zoom) {
+    $config = get_config('mod_zoom');
+    $now = time();
+
+    $firstavailable = $zoom->start_time - ($config->firstabletojoin * 60);
+    $lastavailable = $zoom->start_time + $zoom->duration;
+    $inprogress = ($firstavailable <= $now && $now <= $lastavailable);
+
+    $available = $zoom->type == ZOOM_RECURRING_MEETING || $inprogress;
+
+    $finished = $zoom->type != ZOOM_RECURRING_MEETING && $now > $lastavailable;
+
+    return array($inprogress, $available, $finished);
+}

view.php
@@ -25,8 +25,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Replace zoom with the name of your module and remove this line.
-
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
 require_once(dirname(__FILE__).'/locallib.php');
@@ -116,20 +114,20 @@ $table->attributes['class'] = 'generaltable mod_view';
 $table->align = array('center', 'left');
 $numcolumns = 2;
 
-$now = time();
-$ttostart = $zoom->start_time - $now;
-$ttoend = $zoom->start_time + $zoom->duration - $now;
+list($inprogress, $available, $finished) = zoom_get_state($zoom);
 
-$available = $zoom->type == 3 ||
-        (0 < $ttoend && $ttostart < $config->firstabletojoin * 60);
 if ($available) {
     if ($userishost) {
-        $link = html_writer::link($zoom->start_url, $strstart);
+        $buttonhtml = html_writer::tag('button', $strstart,
+                array('type' => 'submit', 'class' => 'btn btn-success'));
+        $aurl = new moodle_url($zoom->start_url);
     } else {
-        $loadmeetingurl = new moodle_url('/mod/zoom/loadmeeting.php', array('id' => $cm->id));
-        $link = html_writer::link($loadmeetingurl, $strjoin);
+        $buttonhtml = html_writer::tag('button', $strjoin,
+                array('type' => 'submit', 'class' => 'btn btn-primary'));
+        $aurl = new moodle_url('/mod/zoom/loadmeeting.php', array('id' => $cm->id));
     }
-    $link = html_writer::tag('span', $link, array('style' => 'font-size:20px'));
+    $buttonhtml .= html_writer::input_hidden_params($aurl);
+    $link = html_writer::tag('form', $buttonhtml, array('action' => $aurl->out_omit_querystring()));
 } else {
     $link = html_writer::tag('span', $strunavailable, array('style' => 'font-size:20px'));
 }
@@ -174,18 +172,14 @@ $table->data[] = array($strstartvideopart, $strparticipantsvideo);
 $table->data[] = array($straudioopt, $zoom->option_audio);
 
 if ($zoom->type != ZOOM_RECURRING_MEETING) {
-
-    if ($zoom->type != ZOOM_MEETING_EXPIRED) {
-        $now = time();
-        if ($now < $zoom->start_time) {
-            $status = get_string('meeting_not_started', 'mod_zoom');
-        } else if ($zoom->start_time + $zoom->duration < $now) {
-            $status = get_string('meeting_started', 'mod_zoom');
-        } else {
-            $status = get_string('meeting_finished', 'mod_zoom');
-        }
-    } else {
+    if ($zoom->type == ZOOM_MEETING_EXPIRED) {
         $status = get_string('meeting_expired', 'mod_zoom');
+    } else if ($finished) {
+        $status = get_string('meeting_finished', 'mod_zoom');
+    } else if ($inprogress) {
+        $status = get_string('meeting_started', 'mod_zoom');
+    } else {
+        $status = get_string('meeting_not_started', 'mod_zoom');
     }
 
     $table->data[] = array($strstatus, $status);

commit bce4a90a4737e926f39944dbecf91f2d6f0f7b19

Fix join before host not working. * Regression from 6d931892 - don't put 'option_jbh' in 'meetingoptions' array. * Use advcheckbox to allow disabling join before host.

classes/webservice.php
@@ -135,7 +135,6 @@ class mod_zoom_webservice {
      * Find a user via their email.
      *
      * @param string $email
-     * @param int $logintype Optional authentication method, defaults to ZOOM_SNS_SSO.
      * @return bool
      */
     public function user_getbyemail($email) {

mod_form.php
@@ -102,11 +102,12 @@ class mod_zoom_mod_form extends moodleform_mod {
         ), null, get_string('option_audio', 'zoom'));
         $mform->setDefault('option_audio', ZOOM_AUDIO_BOTH);
 
-        // Add meeting options.
+        // Add meeting options. Make sure we pass $appendName as false
+        // so the options aren't nested in a 'meetingoptions' array.
         $mform->addGroup(array(
             // Join before host.
-            $mform->createElement('checkbox', 'option_jbh', '', get_string('option_jbh', 'zoom'))
-        ), 'meetingoptions', get_string('meetingoptions', 'zoom'));
+            $mform->createElement('advcheckbox', 'option_jbh', '', get_string('option_jbh', 'zoom'))
+        ), 'meetingoptions', get_string('meetingoptions', 'zoom'), null, false);
         $mform->addHelpButton('meetingoptions', 'meetingoptions', 'zoom');
 
         // Add meeting id.