d3220f13ef248e1ceca03e19222d76b2107d7b06
Nathan Chou <nchou0@ucla.edu>
2017-05-04 17:19:17 -0700

    Allow exporting session participants (with idnumber) to xls.
    Participant names from the Zoom report are matched with names of
    enrolled users to determine id number. If multiple users have the
    same name, all possible id numbers will be shown.

--- participants.php
+++ participants.php
@@ -25,12 +25,14 @@
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
 require_once(dirname(__FILE__).'/locallib.php');
+require_once(dirname(__FILE__).'/../../lib/accesslib.php');
 require_once(dirname(__FILE__).'/../../lib/moodlelib.php');
 
 list($course, $cm, $zoom) = zoom_get_instance_setup();
 $session = required_param('session', PARAM_INT); // Session.
+$export = optional_param('export', null, PARAM_ALPHA);
 
-$PAGE->set_url('/mod/zoom/participants.php', array('id' => $cm->id, 'session' => $session));
+$PAGE->set_url('/mod/zoom/participants.php', array('id' => $cm->id, 'session' => $session, 'export' => $export));
 
 $strname = $zoom->name;
 $strtitle = get_string('participants', 'mod_zoom');
@@ -56,37 +58,99 @@ if (!($todisplay = $cache->get($zoom->host_id)) || empty($todisplay->sessions[$z
     $cache->set(strval($zoom->host_id), $todisplay);
 }
 
-echo $OUTPUT->header();
-echo $OUTPUT->heading($strname);
-echo $OUTPUT->heading($strtitle, 4);
-
 $participants = $todisplay->sessions[$zoom->meeting_id][$session]->participants;
-if (empty($participants)) {
-    notice(get_string('noparticipants', 'mod_zoom'),
-            new moodle_url('/mod/zoom/report.php', array('id' => $cm->id)));
+
+// Display the headers/etc if we're not exporting, or if there is no data.
+if (empty($export) || empty($participants)) {
+    echo $OUTPUT->header();
+    echo $OUTPUT->heading($strname);
+    echo $OUTPUT->heading($strtitle, 4);
+
+    // Stop if there is no data.
+    if (empty($participants)) {
+        notice(get_string('noparticipants', 'mod_zoom'),
+                new moodle_url('/mod/zoom/report.php', array('id' => $cm->id)));
+        echo $OUTPUT->footer();
+        exit();
+    }
+}
+
+// Loop through each user to generate name->uids mapping.
+$coursecontext = context_course::instance($course->id);
+$enrolled = get_enrolled_users($coursecontext);
+$nametouids = array();
+foreach ($enrolled as $user) {
+    $name = strtoupper(fullname($user));
+    $uids = empty($nametouids[$name]) ? array() : $nametouids[$name];
+    $uids[] = $user->idnumber;
+    $nametouids[$name] = $uids;
 }
 
 $table = new html_table();
-$table->head = array(get_string('name'),
+$table->head = array(get_string('idnumber'),
+                     get_string('name'),
                      get_string('jointime', 'mod_zoom'),
                      get_string('leavetime', 'mod_zoom'),
                      get_string('duration', 'mod_zoom'));
-$numcolumns = 4;
 
 foreach ($participants as $p) {
     $row = array();
-    $row[] = $p->name;
 
+    // For some reason, the Zoom report may add a space to the end of the name.
+    $name = trim($p->name);
+
+    // ID number.
+    $row[] = empty($nametouids[strtoupper($name)]) ? '' : implode(', ', $nametouids[strtoupper($name)]);
+
+    // Name.
+    $row[] = $name;
+
+    // Join/leave times.
     $join = strtotime($p->join_time);
     $row[] = userdate($join);
     $leave = strtotime($p->leave_time);
     $row[] = userdate($leave);
 
+    // Duration.
     $row[] = format_time($leave - $join);
 
     $table->data[] = $row;
 }
 
+if ($export != 'xls') {
+    echo html_writer::table($table);
 
-echo html_writer::table($table);
-echo $OUTPUT->footer();
+    $exporturl = new moodle_url('/mod/zoom/participants.php', array(
+            'id' => $cm->id,
+            'session' => $session,
+            'export' => 'xls'
+        ));
+    $xlsstring = get_string('application/vnd.ms-excel', 'mimetypes');
+    $xlsicon = html_writer::img($OUTPUT->pix_url('f/spreadsheet'), $xlsstring, array('title' => $xlsstring));
+    echo get_string('export', 'mod_zoom') . ': ' . html_writer::link($exporturl, $xlsicon);
+
+    echo $OUTPUT->footer();
+} else {
+    require_once(dirname(__FILE__).'/../../lib/excellib.class.php');
+
+    $workbook = new MoodleExcelWorkbook("zoom_participants_{$zoom->meeting_id}");
+    $worksheet = $workbook->add_worksheet($strtitle);
+    $boldformat = $workbook->add_format();
+    $boldformat->set_bold(true);
+    $row = $col = 0;
+
+    foreach($table->head as $colname) {
+        $worksheet->write_string($row, $col++, $colname, $boldformat);
+    }
+    $row++; $col = 0;
+
+    foreach ($table->data as $entry) {
+        foreach ($entry as $value) {
+            $worksheet->write_string($row, $col++, $value);
+        }
+        $row++; $col = 0;
+    }
+
+    $workbook->close();
+    exit();
+}
