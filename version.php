2fa56f173d6a9537adb98a0fcf8d18741b92354f
tatikamsetti <tatikamsetti@users.noreply.github.com>
2018-08-13 20:21:52 -0700

    Updated version number.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2018071900;
+$plugin->version = 2018081300;
 $plugin->release = 'v2.0';
 $plugin->requires = 2017111304.00;
 $plugin->maturity = MATURITY_STABLE;
