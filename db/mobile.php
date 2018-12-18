0a7b9841379bb5c5869dd849fae99ca93eb0049f
nstefanski <nstefanski@escoffier.edu>
2018-12-18 16:43:22 -0600

    3.5 mobile support
    Added support for viewing and joining meetings via mobile app in 3.5+
    
    See https://docs.moodle.org/dev/Mobile_support_for_plugins for more details on mobile support in 3.5
    
    Does not currently support features specific to host users, ie displaying the start link instead of join and meeting password
    
    Does not currently support the option to view all meetings in a course -- this will probably need a new webservice in /zoom/classes/external.php

--- db/mobile.php
+++ db/mobile.php
@@ -18,13 +18,42 @@
  * Zoom module capability definition
  *
  * @package    mod_zoom
- * @author     Nick Stefanski <nstefanski@escoffier.edu>
- * @copyright  2017 Auguste Escoffier School of Culinary Arts {@link https://www.escoffier.edu}
+ * @copyright  2018 Nick Stefanski <nmstefanski@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
-
 $addons = array(
-    "mod_zoom" => array()
+  "mod_zoom" => array(
+		"handlers" => array(
+			'zoommeetingdetails' => array(
+				'displaydata' => array(
+				'title' => 'pluginname',
+					'icon' => $CFG->wwwroot . '/mod/zoom/pix/icon.gif',
+					'class' => '',
+				),
+				
+				'delegate' => 'CoreCourseModuleDelegate',
+				'method' => 'mobile_course_view', // main function in \mod_zoom\output\mobile
+				'offlinefunctions' => array(
+					'mobile_course_view' => array(),
+				),
+			)
+		),
+		'lang' => array(
+			array('pluginname', 'zoom'),
+			array('join_meeting', 'zoom'),
+			array('unavailable', 'zoom'),
+			array('meeting_time', 'zoom'),
+			array('duration', 'zoom'),
+			array('passwordprotected', 'zoom'),
+			array('password', 'zoom'),
+			array('join_link', 'zoom'),
+			array('joinbeforehost', 'zoom'),
+			array('starthostjoins', 'zoom'),
+			array('startpartjoins', 'zoom'),
+			array('option_audio', 'zoom'),
+			array('status', 'zoom'),
+			array('recurringmeetinglong', 'zoom')
+		)
+	)
 );
