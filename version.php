d873c648ffbfaa106e7466b91e3fbb052f6b8cbc
Dan Marsden <dan@danmarsden.com>
2019-04-16 09:49:04 +1200

    Added userlist functions for privacy api (#53)
    * Fix #51 - add userlist functions for privacy api
    fixes unit test failures.
    * Fix #54 correct userid field type.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2018092200;
+$plugin->version = 2018092201;
 $plugin->release = 'v2.0.1';
 $plugin->requires = 2017051500.00;
 $plugin->maturity = MATURITY_STABLE;
