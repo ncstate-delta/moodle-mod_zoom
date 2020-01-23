67f7bfc4a03e40fdb368719515ae6af56119372c
Naveen <naveen@linkstreet.in>
2020-01-23 15:28:24 +0530

    TASK: Bump plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2020012300;
+$plugin->version = 2020012301;
 $plugin->release = 'v2.2.4';
 $plugin->requires = 2017051500.00;
 $plugin->maturity = MATURITY_STABLE;
