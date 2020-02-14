115b53db026880ce347a402ca6cf3604d922e7d2
Nayana <nayana@linkstreet.in>
2020-02-14 12:09:34 +0530

    [TASK]: Updated the zoom recordings

--- classes/task/insert_recordings.php
+++ classes/task/insert_recordings.php
@@ -5,6 +5,10 @@ namespace mod_zoom\task;
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/mod/zoom/locallib.php');
+require_once($CFG->dirroot.'/lib/modinfolib.php');
+require_once($CFG->dirroot.'/mod/zoom/lib.php');
+require_once($CFG->dirroot.'/mod/zoom/classes/webservice.php');
+
 
 /**
  * Scheduled task to sychronize meeting data.
@@ -30,43 +34,36 @@ require_once($CFG->dirroot.'/mod/zoom/locallib.php');
      * @return boolean
      */
     public function execute() {
+        
         global $CFG, $DB;
         $config = get_config('mod_zoom');
-        require_once($CFG->dirroot.'/lib/modinfolib.php');
-        require_once($CFG->dirroot.'/mod/zoom/lib.php');
-        require_once($CFG->dirroot.'/mod/zoom/classes/webservice.php');
-
-        $sql = "SELECT e.*, mz.meeting_id from mdl_event as e
+       
+        $sql = "SELECT e.*, mz.meeting_id, mz.auto_recording from mdl_event as e
                 join mdl_zoom mz on e.instance = mz.id
                 where e.modulename = 'zoom'and e.recording_created = 0
-                and mz.deleted_at IS NULL and e.endtime < now()";
+                and mz.deleted_at IS NULL and e.endtime < UNIX_TIMESTAMP(NOW())";
 
         $zoom_events = $DB->get_records_sql($sql);
+        $service = new \mod_zoom_webservice();
         foreach ($zoom_events as $value) {
-            $service = new \mod_zoom_webservice();
             try {
-            $recordings = $service->get_meeting_recording($value->meeting_id);
+                $recordings = $service->get_meeting_recording($value->meeting_id);
             } catch (\moodle_exception $error) {
                 throw zoom_is_meeting_gone_error($error);
             }
-             var_dump($recordings);
-        }
-
-        if (!empty($recordings)) {
-            $rec = $recordings->recording_files{0};
-            $record = new\stdClass();
-            $record->meeting_id = $recordings->id;
-            $record->uuid = $recordings->uuid;
-            $record->play_url = $rec->play_url;
-            $record->download_url = $rec->download_url;
-            $record->start_time = $rec->recording_start;
-            $record->end_time = $rec->recording_end;
-            $record->status = $rec->status;
-            var_dump($record);
-            if($rec->status == 'completed'){
+            if (!empty($recordings)) {
+                $rec = $recordings->recording_files{0};
+                $record = new\stdClass();
+                $record->meeting_id = $recordings->id;
+                $record->uuid = $recordings->uuid;
+                $record->play_url = $rec->play_url;
+                $record->download_url = $rec->download_url;
+                $record->start_time = $rec->recording_start;
+                $record->end_time = $rec->recording_end;
+                $record->status = $rec->status;
                 $DB->insert_record('zoom_recordings',$record);
-            }   
-        }
-            
-	}	
+                $DB->update_record('event', (object)['id' => $value->id, 'recording_created' => 1]);
+            }
+        }  
+    }
 }
