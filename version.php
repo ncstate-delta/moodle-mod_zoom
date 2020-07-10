0d85255f121de9268c179234ad99dfddd25597e5
Michael Hughes <michael@phoenxproductions.org.uk>
2020-07-10 22:54:46 +0100

    Unbumped version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2020061201;
+$plugin->version = 0;
 $plugin->release = 'v3.1';
 $plugin->requires = 2017051500.00;
 $plugin->maturity = MATURITY_STABLE;
