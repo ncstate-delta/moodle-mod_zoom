652adf7cb7050b2a57346988cb4ea89e559e0af8
tzerafnx <tzerafnx@gmail.com>
2020-08-13 18:13:31 -0400

    update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2020061200;
+$plugin->version = 2020081200;
 $plugin->release = 'v3.1';
 $plugin->requires = 2017051500.00;
 $plugin->maturity = MATURITY_STABLE;
