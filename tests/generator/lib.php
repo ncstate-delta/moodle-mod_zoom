a4b04c9efb5c6d4c653be8e3646b59d8fa7c9e41
Jack Van Boening <jackvanboening@gmail.com>
2021-02-23 12:26:51 -0800

    CCLE-9618 - Fix lint errors.

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -31,7 +31,7 @@ class mod_zoom_generator extends testing_module_generator {
      * @param array $options
      * @return stdClass Zoom instance
      */
-    public function create_instance($record = null, array $options = NULL) {
+    public function create_instance($record = null, array $options = null) {
         global $CFG, $DB;
         require_once($CFG->dirroot.'/mod/zoom/locallib.php');
 
