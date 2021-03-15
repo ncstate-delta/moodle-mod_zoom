ncstate-delta/moodle-mod_zoom

commit 05092c242f669f383825d01e285a6e5e39b07307

Fixed error when trying to create meeting with e2ee hidden
* If user does not have e2ee then form element wouldn't appear, thus causing form validation error about missing e2ee value.

mod_form.php
@@ -255,10 +255,15 @@ class mod_zoom_mod_form extends moodleform_mod {
             // Check if the user can use e2e encryption.
             $e2eispossible = $securitysettings->end_to_end_encrypted_meetings;
 
-            // Only show if the admin always wants to show this widget or
-            // if the admin wants to show this widget conditionally and the user can use e2e encryption.
-            if ($config->showencryptiontype == ZOOM_ENCRYPTION_ALWAYSSHOW ||
+            if ($config->showencryptiontype == ZOOM_ENCRYPTION_SHOWONLYIFPOSSIBLE && !$e2eispossible) {
+                // If user cannot use e2e and option is not shown to user,
+                // default to enhanced encryption.
+                $mform->addElement('hidden', 'option_encryption_type', ZOOM_ENCRYPTION_TYPE_ENHANCED);
+            } else if ($config->showencryptiontype == ZOOM_ENCRYPTION_ALWAYSSHOW ||
                     ($config->showencryptiontype == ZOOM_ENCRYPTION_SHOWONLYIFPOSSIBLE && $e2eispossible)) {
+                // Only show if the admin always wants to show this widget or
+                // if the admin wants to show this widget conditionally and the user can use e2e encryption.
+                
                 // Add encryption type option, disabled if the user can't use e2e encryption.
                 $encryptionattr = null;
                 $defaultencryptiontype = $config->defaultencryptiontypeoption;
@@ -278,6 +283,7 @@ class mod_zoom_mod_form extends moodleform_mod {
                 $mform->addHelpButton('option_encryption_type_group', 'option_encryption_type', 'zoom');
                 $mform->disabledIf('option_encryption_type_group', 'webinar', 'checked');
             }
+            $mform->setType('option_encryption_type', PARAM_ALPHAEXT);
         }
 
         // Add waiting room widget.

lang/en/zoom.php
@@ -74,7 +74,7 @@ $string['err_password'] = 'Passcode may only contain the following characters: [
 $string['err_password_required'] = 'Passcode is required.';
 $string['err_start_time_past'] = 'The start date cannot be in the past.';
 $string['errorwebservice_badrequest'] = 'Zoom received a bad request: {$a}';
-$string['errorwebservice_notfound'] = 'The resource does not exists: {$a}';
+$string['errorwebservice_notfound'] = 'The resource does not exists';
 $string['errorwebservice'] = 'Zoom webservice error: {$a}.';
 $string['export'] = 'Export';
 $string['firstjoin'] = 'First able to join';
@@ -175,6 +175,7 @@ $string['recurringmeetinglong'] = 'Recurring meeting (meeting with no end date o
 $string['recycleonjoin'] = 'Recycle license upon join';
 $string['refreshreports'] = 'Refresh session reports';
 $string['licenseonjoin'] = 'Select this option if you would like the host to receive a license upon starting the meeting, <i>as well as</i> upon creation.';
+$string['recreatesuccessful'] = 'Sucessfully recreated meeting';
 $string['report'] = 'Reports';
 $string['reportapicalls'] = 'Report API calls exhausted';
 $string['requirepasscode'] = 'Require meeting passcode';

commit 5788e8d0ec7b565bafe04d6c3be4d277fb36dcfe

Fix regression which turned E2E encryption not selectable
There was a regression in commit 05092c242f669f383825d01e285a6e5e39b07307 which set a parameter type of PARAM_ALPHAEXT for the option_encryption_type parameter. However, the E2E encryption has a parameter value of 'e2ee'. As a result, if a user selected E2E encryption during an activity creation, the form submission was denied and the 'The submitted encryption type is not valid' validation error was given.

This patch fixes this regression by simply changing the parameter type to PARAM_ALPHANUMEXT.

mod_form.php
@@ -283,7 +283,7 @@ class mod_zoom_mod_form extends moodleform_mod {
                 $mform->addHelpButton('option_encryption_type_group', 'option_encryption_type', 'zoom');
                 $mform->disabledIf('option_encryption_type_group', 'webinar', 'checked');
             }
-            $mform->setType('option_encryption_type', PARAM_ALPHAEXT);
+            $mform->setType('option_encryption_type', PARAM_ALPHANUMEXT);
         }
 
         // Add waiting room widget.