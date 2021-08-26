59831c72f0c6b91ac7c3e1ca76b108aea2840d2c
Michelle Melton <meltonml@appstate.edu>
2021-08-26 13:45:28 -0400

    Add table for storing tracking field values per meeting

--- db/upgrade.php
+++ db/upgrade.php
@@ -520,11 +520,11 @@ function xmldb_zoom_upgrade($oldversion) {
     }
     
     // Update to current!
-    if ($oldversion < 2021082001) {
+    if ($oldversion < 2021082601) {
         // Define table zoom_tracking_fields to be created.
         $table = new xmldb_table('zoom_tracking_fields');
         
-        // Adding fields to table zoom_meeting_details.
+        // Adding fields to table zoom_tracking_fields.
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('zoomid', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
         $table->add_field('field', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
@@ -539,6 +539,26 @@ function xmldb_zoom_upgrade($oldversion) {
         $table->add_index('field', XMLDB_INDEX_UNIQUE, array('field'));
         
         // Conditionally launch create table for zoom_tracking_fields.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+        
+        // Define table zoom_meeting_tracking_fields to be created.
+        $table = new xmldb_table('zoom_meeting_tracking_fields');
+        
+        // Adding fields to table zoom_meeting_tracking_fields.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('meeting_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('tracking_field_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('value', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        
+        // Adding keys to table zoom_meeting_tracking_fields.
+        $table->add_key('id_primary', XMLDB_KEY_PRIMARY, array('id'));
+        
+        // Adding indexes to table zoom_meeting_tracking_fields.
+        $table->add_index('meeting_id', XMLDB_INDEX_UNIQUE, array('meeting_id'));
+        
+        // Conditionally launch create table for zoom_meeting_tracking_fields.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
