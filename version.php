1547103bbb81fd61e0b5403b8f9f49514ae7fcab
Jonathan Champ <jrchamp@ncsu.edu>
2021-12-09 17:09:01 -0500

    release: v4.3.1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2021112900;
-$plugin->release = 'v4.3';
+$plugin->version = 2021120900;
+$plugin->release = 'v4.3.1';
 $plugin->requires = 2017051500.00;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
