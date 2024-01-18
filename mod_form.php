3a362ff216c4d8e60675f13b4409e47eb12e2cba
Jonathan Champ <jrchamp@ncsu.edu>
2024-01-18 12:01:37 -0500

    breakout rooms: use fullname(); fix JSON trailing comma

--- mod_form.php
+++ mod_form.php
@@ -422,7 +422,10 @@ class mod_zoom_mod_form extends moodleform_mod {
         // Getting Course participants.
         $courseparticipants = [];
         foreach ($participants as $participant) {
-            $courseparticipants[] = ['participantid' => $participant->id, 'participantname' => $participant->firstname . ' ' . $participant->lastname];
+            $courseparticipants[] = [
+                'participantid' => $participant->id,
+                'participantname' => fullname($participant) . ' <' . $participant->email . '>',
+            ];
         }
 
         // Getting Course groups.
