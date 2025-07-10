b93c22281e1569965eea57bf9ea3409ba5427b48
Jonathan Champ <jrchamp@ncsu.edu>
2025-07-10 11:23:11 -0400

    release: v5.4.2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2025070300;
-$plugin->release = 'v5.4.1';
+$plugin->version = 2025071000;
+$plugin->release = 'v5.4.2';
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
